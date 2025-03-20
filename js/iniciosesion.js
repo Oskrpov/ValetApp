a9a557ce09a1782869cf49abaca78c7df4a94288
Oskrpov <oskrpov@gmail.com>
2025-03-19 21:43:30 -0500

    Primer commit

---
+++ js/iniciosesion.js
@@ -0,0 +1,12 @@
+document.addEventListener("click", (e)=>{
+    if(e.target.matches("#iniciar")) location.href="index.html"
+}
+)
+document.addEventListener("click", (e)=>{
+    if(e.target.matches("#imgvalet")) location.href="inicio_sesión.html"
+}
+)
+document.addEventListener("click", (e)=>{
+    if(e.target.matches("#imgadmon")) location.href="inicio_sesión.html"
+}
+)
---
+++ js/main.js
@@ -0,0 +1,16 @@
+document.addEventListener("click", (e)=>{
+    if(e.target.matches("#nuevocliente")) location.href="nuevo_cliente.html"
+}
+)
+document.addEventListener("click", (e)=>{
+    if(e.target.matches("#buscarcliente")) location.href="busqueda_cliente.html"
+}
+)
+document.addEventListener("click", (e)=>{
+    if(e.target.matches("#botonbuscar")) location.href="datos_cliente.html"
+}
+)
+document.addEventListener("click", (e)=>{
+    if(e.target.matches("#continuarnovedades")) location.href="datos_cliente.html"
+}
+)
