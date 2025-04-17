fa6fb4fec413c141b7d559894f10ff5dc097eef0
Oskrpov <oskrpov@gmail.com>
2025-04-16 23:10:58 -0500

    configurado el login en javascrip

--- js/iniciosesion.js
+++ js/iniciosesion.js
@@ -1,12 +1,43 @@
-document.addEventListener("click", (e)=>{
-    if(e.target.matches("#iniciar")) location.href="index.html"
+document.getElementById("loginForm").addEventListener("submit", function (e) {
+    e.preventDefault();
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+    if (username.trim() === "" || password.trim() === "") {
+      alert("Por favor, completa todos los campos.");
+      return;
+    }
+    
+    console.log("Credenciales ingresadas:", {username, password});
+
+  });
+  function url(destino){
+    location.href.destino
+  }
+
+  document.addEventListener("submit", (e)=>{
+    e.preventDefault()
+    if(e.target.matches("#loginForm")) validarlogin()
+  })
+
+  function validarlogin() {  
+
+      let ExReg_mail = /^[^@]+@[^@]+\.[a-zA-Z]{2,}$/
+    let ExRegContraseña = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{4,18}$/
+
+    let $div_msg = document.querySelector("#div_msg"),msg=""
+    $div_msg.innerHTML="Procesando..."
+
+    let usuario = username.value ,pass = password.value
+    console.log("user: "+usuario,"password: "+pass)
+    if(!ExReg_mail.test(usuario)){
+        msg="Correo invalido"
+        if(!ExRegContraseña.test(pass)){
+            msg="Correo y Contraseña invalidos"
+    }
+  } else if(!ExRegContraseña.test(pass)){
+        msg="Contraseña invalida"
+    }
+    if(!msg=="") {$div_msg.innerHTML="<b>"+msg+"</b>";return false;}
+    $div_msg.innerHTML="datos ingrsados correctamente";
 }
-)
-document.addEventListener("click", (e)=>{
-    if(e.target.matches("#imgvalet")) location.href="inicio_sesion.html"
-}
-)
-document.addEventListener("click", (e)=>{
-    if(e.target.matches("#imgadmon")) location.href="inicio_sesion.html"
-}
-)
